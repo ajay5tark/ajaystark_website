b27b19c464881fe34e2999e1104ef1d0830272d5
Ajay Prakash <ajay18215@iiitd.ac.in>
2019-07-05 06:25:27 +0530

    removed sqlite file

--- groot/groot/urls.py
+++ groot/groot/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from ajaystark import urls
-from share import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
